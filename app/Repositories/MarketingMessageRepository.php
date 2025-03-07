daa533867b72b6f34a7d58dbf26da194b837b340
m7moud <mg6783256@gmail.com>
2025-03-07 06:40:54 +0200

    done

--- app/Repositories/MarketingMessageRepository.php
+++ app/Repositories/MarketingMessageRepository.php
@@ -45,10 +45,28 @@ class MarketingMessageRepository
     public function updateMarketingMessage($id, array $data)
     {
         $marketingMessage = MarketingMessage::find($id);
-        if ($marketingMessage) {
+        
+        if ($marketingMessage) 
+        {
+            $ids = is_array($data['client_ids']) ? $data['client_ids'] : json_decode($data['client_ids']);
             $data['client_ids'] = is_array($data['client_ids']) ? json_encode($data['client_ids']) : $data['client_ids'];
-            $data['attachment_path'] = isset($data['attachment_path']) ? HandleUpload::uploadFile($data['attachment_path'],'attachments') : $marketingMessage->attachment_path;
+            $data['delivery_method'] = is_array($data['delivery_method']) ? json_encode($data['delivery_method']) : $data['delivery_method'];
+            $data['attachment_path'] = isset($data['attachment_path']) ? HandleUpload::uploadFile($data['attachment_path'],'attachments') : NULL;
+
+            $ids = is_array($ids) ? $ids : [$ids];
+            $clients = $data['send_to'] == 'all' ? Client::all() : Client::whereIn('id',$ids)->get();
+
+            foreach($clients ?? [] as $client)
+            {
+                $link = isset($data['attachment_path']) ? Storage::disk('public')->url($data['attachment_path']) : NULL;
+
+                $message =  $data['message_body'] . " " . $link;
+
+                $response = SendMessageHelper::SendMessage($client->phone,$message);
+            }
+            
             $marketingMessage->update($data);
+
             return $marketingMessage;
         }
         return null;
